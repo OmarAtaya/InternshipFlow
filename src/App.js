34956e280fc9f0952a6fdfafeece10990c6c498a
OmarAtaya <omar_ataya@outlook.com>
2022-10-23 14:03:35 +0300

    Completed changes to sideBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Graph from './Components/graph';
+import Sidebar from './Components/sideBar';
+import Image from './Images/add-square.png';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Internships
-      </header>
+      <div className="App-content">
+        <div className="App-title">
+          <h1 className="title">Internships</h1>
+          <button className="App-button">
+            <img src={Image} className="Button-image" alt=""/>
+            <p>Create New Internship</p>
+          </button>
+        </div>
+        <Graph/>
+      </div>
+      <Sidebar/>
+      
     </div>
   );
 }
--- src/Components/sideBar.js
+++ src/Components/sideBar.js
@@ -1,22 +1,42 @@
 import './sideBar.css';
+import radicalx from '../Images/RadicallX-Black-Logo 1.png'
+import dashboard from '../Images/dashboard-3.png'
+import apprentership from '../Images/medal-star.png'
+import internship from '../Images/book.png'
+import job from '../Images/briefcase.png'
+import setting from '../Images/setting-2.png'
+import account from '../Images/Rectangle 18895.png'
 
 export default function Sidebar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Sidebar">
+        <div className="Sidebar-menu" alt="">
+            <img src={radicalx} className="Sidebar-logo" alt=""/>
+            <div className="Sidebar-dashboard">
+                <img src={dashboard} className="Sidebar-icon" alt=""/>
+                <p>Dashboard</p>
+            </div>
+            <div className="Sidebar-apprenterships">
+                <img src={apprentership} className="Sidebar-icon" alt=""/>
+                <p>Apprenterships</p>
+            </div>
+            <div className="Sidebar-internships">
+                <img src={internship} className="Sidebar-icon" alt=""/>
+                <p>Internships</p>
+            </div>
+            <div className="Sidebar-jobs">
+                <img src={job} className="Sidebar-icon" alt=""/>
+                <p>Jobs</p>
+            </div>
+            <div className="Sidebar-settings">
+                <img src={setting} className="Sidebar-icon" alt=""/>
+                <p>Settings</p>
+            </div>
+      </div>
+      <div className="Sidebar-account">
+        <img src={account} className="Account-profile" alt=""/>
+        <p>Adam Scott</p>
+      </div>
     </div>
   );
 }
